221bd54c5ca5173cf701f07602aaad75ebede031
Danny Chang <dannychachang@gmail.com>
2018-02-05 17:17:26 -0500

    Finish format assignment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
   def index
     respond_to do |format|
       format.html
+      format.text
+      format.csv do
+        render plain: Book.generate_csv(Book.all)
+      end
+      format.json do
+        render json: Book.all
+      end
     end
   end
 
